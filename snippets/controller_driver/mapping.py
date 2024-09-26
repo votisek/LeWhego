2768021ca959442df431d3c26f3a63f68b557fc5
votisek <votisek.pog@gmail.com>
2024-09-26 18:46:00 +0200

    neco 1

---
+++ snippets/controller_driver/mapping.py
@@ -0,0 +1,26 @@
+mapping = {
+    
+}
+""" 
+################
+# TRANSMISSION #
+################
+
+# TRANSISSION_UP
+# TRANSMISSION_DOWN
+# TRANSMISSION_RESET
+
+##########
+# PEDALS #
+##########
+
+# BRAKE_0
+# BRAKE_1
+# BRAKE_2
+# BRAKE_3
+# BRAKE_4
+# HAND_BRAKE
+# CLUTCH
+# 
+
+"""
--- snippets/translator/__init__.py
+++
@@ -1,6 +0,0 @@
-import keyboard as keb
-def get_from_mapping(mapping, key):
-    return mapping[key]
-
-def translate(cntr_press, mapping: dict):
-    pass
